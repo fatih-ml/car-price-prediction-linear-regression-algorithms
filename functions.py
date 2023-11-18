d3a081e38e7db29be242ce82433047775d1a6b5b
fatsibumi <fatsibumi@gmail.com>
2023-11-19 01:40:22 +0300

    feature selection

--- functions.py
+++ functions.py
@@ -55,3 +55,37 @@ def adjusted_r2_score(y_true, y_pred, n, k):
     r2 = r2_score(y_true, y_pred)
     adjusted_r2 = 1 - ((1 - r2) * (n - 1) / (n - k - 1))
     return adjusted_r2
+
+def residual_analysis_df(df, y_test, y_pred, cols , log_transformed=True):
+    """
+    This function, is to make a comparison and analysis dataframe of residuals
+    
+    (df): it takes a dataframe df where the observations are stored (generally called X)
+    
+    (cols): it takes cols --> array or list of column names from dataframe X 
+        to add analysis dataframe fo comparison
+    
+    it takes y_test and y_pred, generally numpy arrays
+    
+    it takes log_transformed: bool, default=True if target feature is log transformed
+    
+    returns a dataframe, where each row is X_test[cols] + y_test + y_pred + residuals
+        with a sorted view by absolute value of residuals
+    """  
+    if log_transformed:
+        y_test = np.exp(y_test)
+        y_pred = np.exp(y_pred)
+    residuals = y_test - y_pred
+    indexes = y_test.index
+    comp_df = pd.DataFrame(columns=['Real_Price', 'Predicted_Price', 'Residuals'], index=indexes)
+    comp_df['Real_Price'] = y_test
+    comp_df['Predicted_Price'] = y_pred
+    comp_df['Residuals'] = np.abs(residuals)
+    comp_df.sort_values(by='Residuals', inplace=True, ascending=False)
+    comp_indexes = comp_df.index
+    for col in cols:
+        comp_df[col] = pd.Series()
+    for comp_index in comp_indexes:
+        comp_df.loc[comp_df.index==comp_index, cols] = df[df.index==comp_index][cols].values
+        
+    return comp_df
